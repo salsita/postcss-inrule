f3af2fc559b7e0b3d47328b39f9771843a526fd3
mystrdat <mystrdat@gmail.com>
2016-10-23 17:11:26 +0200

    more tests

--- test.js
+++ test.js
@@ -11,13 +11,37 @@ function run(t, input, output, opts = {}) {
 }
 
 test('Append to parent selector', t => {
-  return run(t, 'body{div{h2{@in <.mod{prop: value;}}}}', 'body{div{h2{}}}body{div.mod{h2{prop: value}}}'); // eslint-disable-line max-len
+  return run(t,
+    'body{div{h2{@in <.mod{prop: value;}}}}',
+    'body{div{h2{}}}body{div.mod{h2{prop: value}}}');
+});
+
+test('Append to parent multiselector', t => {
+  return run(t,
+    'body{div, span{h2{@in <.mod{prop: value;}}}}',
+    'body{div, span{h2{}}}body{div.mod, span.mod{h2{prop: value}}}');
 });
 
 test('Insert in parent selector', t => {
-  return run(t, 'body{div{h2{@in ^.mod{prop: value;}}}}', 'body{div{h2{}}}body{div .mod{h2{prop: value}}}'); // eslint-disable-line max-len
+  return run(t,
+    'body{div{h2{@in ^.mod{prop: value;}}}}',
+    'body{div{h2{}}}body{div .mod{h2{prop: value}}}');
+});
+
+test('Insert in parent multiselector', t => {
+  return run(t,
+    'body{div, span{h2{@in ^.mod{prop: value;}}}}',
+    'body{div, span{h2{}}}body{div .mod, span .mod{h2{prop: value}}}');
 });
 
 test('Replace in parent selector', t => {
-  return run(t, 'body{div{h2{@in @.mod{prop: value;}}}}', 'body{div{h2{}}}body{.mod{h2{prop: value}}}'); // eslint-disable-line max-len
+  return run(t,
+    'body{div{h2{@in @.mod{prop: value;}}}}',
+    'body{div{h2{}}}body{.mod{h2{prop: value}}}');
+});
+
+test('Replace in parent multiselector', t => {
+  return run(t,
+    'body{div, span{h2{@in @.mod{prop: value;}}}}',
+    'body{div, span{h2{}}}body{.mod{h2{prop: value}}}');
 });
